869aa58f1bf0a77cdf881d078d19493b5c11418d
warmtan <1203016783@qq.com>
2022-11-03 10:54:08 +0800

    update

---
+++ GripperTestPython/GripperTestPython/dh_client_socket.py
@@ -0,0 +1,41 @@
+import sys
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+class dh_client_socket(object):
+
+    def connect_device(self, host_add, port) :
+        ret = -1
+        
+        ret = client_socket.connect_ex((host_add,port))
+        print('Connect recv: ',ret)
+        if(ret < 0) :
+            print('Connect Error')
+            ret = -1
+
+        else :
+            print('Connect Success')
+            ret = 0
+        return ret
+
+    def disconnect_device() :
+        client_socket.close()
+        return True
+
+    def device_wrire(self, nDate) :
+        length = 0
+        date = bytes(nDate)
+        length = client_socket.send(date)
+        #print('length: ', length)
+        return length
+
+    def device_read(self, length) :
+        date = client_socket.recv(length)
+        #print('recv: ', date.hex())
+        return date
+
+
+    """description of class"""
+
+
